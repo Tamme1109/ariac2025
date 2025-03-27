3ffa710c6c6d14fdb32609ccf20d9d2037bb0c1a
Tamme1109 <tamme1109@gmail.com>
2025-03-27 14:17:18 +0100

    agv controller with a script that moves it, doesnt fully work, but we working on it

--- ariac_kandidat/scripts/agv_test.py
+++ ariac_kandidat/scripts/agv_test.py
@@ -5,17 +5,29 @@ import threading
 from rclpy.executors import MultiThreadedExecutor
 from ariac_kandidat.competition_interface import CompetitionInterface
 from ariac_msgs.msg import CompetitionState
+from ariac_kandidat.agv_controller import AGVController
+
 
 def main(args=None):
     rclpy.init(args=args)
+
+    # Spawn all neccessary nodes, in this case only the agv for testing
+    agv_control = AGVController()
     interface = CompetitionInterface()
+
+
     executor = MultiThreadedExecutor()
     executor.add_node(interface)
+    executor.add_node(agv_control)
 
     spin_thread = threading.Thread(target=executor.spin)
     spin_thread.start()
 
     interface.start_competition()
+
+    agv_control.lock_agv_tray(1)
+    agv_control.move_agv(1)
+    
 
     while rclpy.ok():
         try:
